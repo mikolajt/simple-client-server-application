c26986ac8092667525cd918c257c4a6586f40e16
mikolajt <mikolajtopolski@gmail.com>
2017-11-04 13:10:15 +0100

    Add files via upload

---
+++ Client/Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient client = new TcpClient("127.0.0.1", 3000);
+                Console.WriteLine("Nawiazano Polaczenie");
+
+                BinaryWriter writer = new BinaryWriter(client.GetStream());
+                BinaryReader reader = new BinaryReader(client.GetStream());
+
+                string text = reader.ReadString();
+                Console.WriteLine("Odebrano tekst: " + text);
+                byte[] ASCIIbyte = Encoding.ASCII.GetBytes(text);
+                int sum = 0;
+                foreach (byte b in ASCIIbyte)
+                {
+                    sum += b;
+                }
+                writer.Write(sum);
+                Console.WriteLine("Wyslano dane: " + sum);
+
+                client.Close();
+                Console.WriteLine("Zamknieto polaczenie");
+            }
+            catch (SocketException) { Console.WriteLine("Blad: Nie udalo sie polaczyc z serwerem"); }
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 3000);
+                server.Start();
+                TcpClient newClient = server.AcceptTcpClient();
+                Console.WriteLine("Nawiazano polaczenie");
+
+                BinaryWriter writer = new BinaryWriter(newClient.GetStream());
+                BinaryReader reader = new BinaryReader(newClient.GetStream());
+
+                string text;
+                Console.WriteLine("Podaj tekst: ");
+                text = Console.ReadLine();
+                writer.Write(text);
+                Console.WriteLine("Wyslano Tekst");
+
+                Console.WriteLine("Suma znakow ASCII wynosi " + reader.ReadInt16());
+            }
+            catch (SocketException) { Console.WriteLine("Blad"); }
+        }
+    }
+}
